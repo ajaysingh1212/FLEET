1c5547b9ec472e899e53c4eecbefd30651637fb3
ajay kumar <ajaykumarwrs1997@gmail.com>
2026-02-03 14:48:24 +0530

    safafc

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Live Vehicle Trackings</title>
+    <title>Live Vehicle Trackingssdc</title>
 
     <style>
         body { margin:0; font-family: Arial, sans-serif; }
